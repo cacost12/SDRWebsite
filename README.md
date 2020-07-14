d13118cd92272e3586180a8128ec22c345d9c6e9
Colton Acosta <cacost12@asu.edu>
2020-07-14 12:25:03 -0700

    Test Commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# SDRWebsite
+# This is a testing repository for practicing using Git
+# Any files uploaded to this directory will be indiscriminately deleted
---
+++ testCode.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	printf("This is a test program \n \n");
+	printf("Wow it worked!\n");
+
+
+
+	return 0;
+}
